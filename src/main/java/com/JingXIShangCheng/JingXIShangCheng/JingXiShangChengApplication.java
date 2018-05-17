97f1feeca1330f7e0494138dde0e2cde5470bce0
Lei Wang <lewang@thoughtworks.com>
2018-05-17 11:19:31 +0800

    init

---
+++ src/main/java/com/JingXIShangCheng/JingXIShangCheng/JingXiShangChengApplication.java
@@ -0,0 +1,12 @@
+package com.JingXIShangCheng.JingXIShangCheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingXiShangChengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingXiShangChengApplication.class, args);
+	}
+}
